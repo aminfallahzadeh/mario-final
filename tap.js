86a05f2cee378f90a3556d51819d40144b50aa0a
amin-dev-tech <amin.dev.tech@gmail.com>
2024-07-08 00:28:38 +0300

    add footer

--- tap.js
+++ tap.js
@@ -1,24 +1,61 @@
-// TAP TO EARN EFFECT
+// // TAP TO EARN EFFECT
+// const img = document.getElementById("tap-effect");
+
+// img.addEventListener("touchstart", (event) => {
+//   event.preventDefault();
+
+//   const rect = img.getBoundingClientRect();
+//   const imgX = rect.left + rect.width / 2;
+//   const imgY = rect.top + rect.height / 2;
+
+//   img.style.transformOrigin = "center center";
+//   img.style.transition = "transform 0.15s ease";
+
+//   const threshold = rect.width * 0.1;
+
+//   Array.from(event.touches).forEach((touch) => {
+//     const touchX = touch.clientX;
+//     const touchY = touch.clientY;
+
+//     img.style.transform = "scale(0.90)";
+
+//     const plusOne = document.createElement("div");
+//     plusOne.className = "plus-one";
+//     plusOne.innerText = "+1";
+//     document.body.appendChild(plusOne);
+
+//     plusOne.style.position = "absolute";
+//     plusOne.style.left = `${touchX}px`;
+//     plusOne.style.top = `${touchY}px`;
+
+//     setTimeout(() => {
+//       img.style.transform = "rotateY(0deg) rotateX(0deg) scale(1)";
+//     }, 100);
+
+//     setTimeout(() => {
+//       plusOne.remove();
+//     }, 1000);
+//   });
+// });
+
 const img = document.getElementById("tap-effect");
 
-img.addEventListener("touchstart", function (event) {
+img.addEventListener("touchstart", (event) => {
   event.preventDefault();
 
-  const img = this;
   const rect = img.getBoundingClientRect();
-  const imgX = rect.left + rect.width / 2;
-  const imgY = rect.top + rect.height / 2;
-
   img.style.transformOrigin = "center center";
   img.style.transition = "transform 0.15s ease";
 
-  const threshold = rect.width * 0.1;
+  img.style.transform = "scale(0.90)";
+});
 
-  Array.from(event.touches).forEach((touch) => {
+img.addEventListener("touchend", (event) => {
+  const rect = img.getBoundingClientRect();
+
+  Array.from(event.changedTouches).forEach((touch) => {
     const touchX = touch.clientX;
     const touchY = touch.clientY;
-
-    img.style.transform = "scale(0.90)";
 
     const plusOne = document.createElement("div");
     plusOne.className = "plus-one";
@@ -29,9 +66,7 @@ img.addEventListener("touchstart", function (event) {
     plusOne.style.left = `${touchX}px`;
     plusOne.style.top = `${touchY}px`;
 
-    setTimeout(() => {
-      img.style.transform = "rotateY(0deg) rotateX(0deg) scale(1)";
-    }, 100);
+    img.style.transform = "scale(1)";
 
     setTimeout(() => {
       plusOne.remove();
