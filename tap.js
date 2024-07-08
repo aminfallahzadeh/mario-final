d436b51cf581f9f9d7963548ff261cbb623ba095
amin-dev-tech <amin.dev.tech@gmail.com>
2024-07-08 22:41:47 +0300

    work on tap animation

--- tap.js
+++ tap.js
@@ -1,20 +1,62 @@
+// const img = document.getElementById("tap-effect");
+// let touchTimeout;
+
+// img.addEventListener("touchstart", (event) => {
+//   event.preventDefault();
+//   img.style.transform = "scale(0.95)";
+// });
+
+// img.addEventListener("touchend", (event) => {
+//   event.preventDefault();
+
+//   Array.from(event.changedTouches).forEach((touch) => {
+//     const touchX = touch.clientX;
+//     const touchY = touch.clientY;
+
+//     img.style.transform = "scale(1)";
+
+//     const plusOne = document.createElement("div");
+//     plusOne.className = "plus-one";
+//     plusOne.innerText = "+1";
+//     document.body.appendChild(plusOne);
+
+//     plusOne.style.left = `${touchX}px`;
+//     plusOne.style.top = `${touchY}px`;
+
+//     setTimeout(() => {
+//       plusOne.remove();
+//     }, 1000);
+//   });
+// });
+
 const img = document.getElementById("tap-effect");
-let touchTimeout;
+let touchCount = 0;
 
 img.addEventListener("touchstart", (event) => {
   event.preventDefault();
-  // clearTimeout(touchTimeout);
-  img.style.transform = "scale(0.95)";
+  touchCount += event.touches.length;
+
+  if (touchCount === event.touches.length) {
+    requestAnimationFrame(() => {
+      img.style.transform = "scale(0.95)";
+    });
+  }
 });
 
 img.addEventListener("touchend", (event) => {
   event.preventDefault();
 
+  touchCount -= event.changedTouches.length;
+
+  if (touchCount === 0) {
+    requestAnimationFrame(() => {
+      img.style.transform = "scale(1)";
+    });
+  }
+
   Array.from(event.changedTouches).forEach((touch) => {
     const touchX = touch.clientX;
     const touchY = touch.clientY;
-
-    img.style.transform = "scale(1)";
 
     const plusOne = document.createElement("div");
     plusOne.className = "plus-one";
@@ -28,8 +70,4 @@ img.addEventListener("touchend", (event) => {
       plusOne.remove();
     }, 1000);
   });
-
-  // touchTimeout = setTimeout(() => {
-  //   img.style.transform = "scale(1)";
-  // }, 50);
 });
