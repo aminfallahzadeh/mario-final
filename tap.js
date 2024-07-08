74f29fb54ab8bc433bd9f985961a16fa0ee971c7
amin-dev-tech <amin.dev.tech@gmail.com>
2024-07-08 16:05:28 +0300

    add footer

--- tap.js
+++ tap.js
@@ -1,61 +1,23 @@
 // TAP TO EARN EFFECT
-const img = document.getElementById("tap-effect");
-
-img.addEventListener("touchstart", (event) => {
-  event.preventDefault();
-
-  const rect = img.getBoundingClientRect();
-  const imgX = rect.left + rect.width / 2;
-  const imgY = rect.top + rect.height / 2;
-
-  img.style.transformOrigin = "center center";
-  img.style.transition = "transform 0.15s ease";
-
-  const threshold = rect.width * 0.1;
-
-  Array.from(event.touches).forEach((touch) => {
-    const touchX = touch.clientX;
-    const touchY = touch.clientY;
-
-    img.style.transform = "scale(0.90)";
-
-    const plusOne = document.createElement("div");
-    plusOne.className = "plus-one";
-    plusOne.innerText = "+1";
-    document.body.appendChild(plusOne);
-
-    plusOne.style.position = "absolute";
-    plusOne.style.left = `${touchX}px`;
-    plusOne.style.top = `${touchY}px`;
-
-    setTimeout(() => {
-      img.style.transform = "rotateY(0deg) rotateX(0deg) scale(1)";
-    }, 100);
-
-    setTimeout(() => {
-      plusOne.remove();
-    }, 1000);
-  });
-});
-
 // const img = document.getElementById("tap-effect");
 
 // img.addEventListener("touchstart", (event) => {
 //   event.preventDefault();
 
 //   const rect = img.getBoundingClientRect();
+//   const imgX = rect.left + rect.width / 2;
+//   const imgY = rect.top + rect.height / 2;
+
 //   img.style.transformOrigin = "center center";
 //   img.style.transition = "transform 0.15s ease";
 
-//   img.style.transform = "scale(0.95)";
-// });
+//   const threshold = rect.width * 0.1;
 
-// img.addEventListener("touchend", (event) => {
-//   const rect = img.getBoundingClientRect();
-
-//   Array.from(event.changedTouches).forEach((touch) => {
+//   Array.from(event.touches).forEach((touch) => {
 //     const touchX = touch.clientX;
 //     const touchY = touch.clientY;
+
+//     img.style.transform = "scale(0.90)";
 
 //     const plusOne = document.createElement("div");
 //     plusOne.className = "plus-one";
@@ -66,10 +28,48 @@ img.addEventListener("touchstart", (event) => {
 //     plusOne.style.left = `${touchX}px`;
 //     plusOne.style.top = `${touchY}px`;
 
-//     img.style.transform = "scale(1)";
+//     setTimeout(() => {
+//       img.style.transform = "rotateY(0deg) rotateX(0deg) scale(1)";
+//     }, 100);
 
 //     setTimeout(() => {
 //       plusOne.remove();
 //     }, 1000);
 //   });
 // });
+
+const img = document.getElementById("tap-effect");
+
+img.addEventListener("touchstart", (event) => {
+  event.preventDefault();
+
+  const rect = img.getBoundingClientRect();
+  img.style.transformOrigin = "center center";
+  img.style.transition = "transform 0.15s ease";
+
+  img.style.transform = "scale(0.95)";
+});
+
+img.addEventListener("touchend", (event) => {
+  const rect = img.getBoundingClientRect();
+
+  Array.from(event.changedTouches).forEach((touch) => {
+    const touchX = touch.clientX;
+    const touchY = touch.clientY;
+
+    const plusOne = document.createElement("div");
+    plusOne.className = "plus-one";
+    plusOne.innerText = "+1";
+    document.body.appendChild(plusOne);
+
+    plusOne.style.position = "absolute";
+    plusOne.style.left = `${touchX}px`;
+    plusOne.style.top = `${touchY}px`;
+
+    img.style.transform = "scale(1)";
+
+    setTimeout(() => {
+      plusOne.remove();
+    }, 1000);
+  });
+});
