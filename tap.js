aeed88ce3d9afbe22d366a4562d2b43cf5ec9199
amin-dev-tech <amin.dev.tech@gmail.com>
2024-07-08 22:46:10 +0300

    work on tap animation

--- tap.js
+++ tap.js
@@ -1,58 +1,14 @@
-// const img = document.getElementById("tap-effect");
-// let touchTimeout;
-
-// img.addEventListener("touchstart", (event) => {
-//   event.preventDefault();
-//   img.style.transform = "scale(0.95)";
-// });
-
-// img.addEventListener("touchend", (event) => {
-//   event.preventDefault();
-
-//   Array.from(event.changedTouches).forEach((touch) => {
-//     const touchX = touch.clientX;
-//     const touchY = touch.clientY;
-
-//     img.style.transform = "scale(1)";
-
-//     const plusOne = document.createElement("div");
-//     plusOne.className = "plus-one";
-//     plusOne.innerText = "+1";
-//     document.body.appendChild(plusOne);
-
-//     plusOne.style.left = `${touchX}px`;
-//     plusOne.style.top = `${touchY}px`;
-
-//     setTimeout(() => {
-//       plusOne.remove();
-//     }, 1000);
-//   });
-// });
-
 const img = document.getElementById("tap-effect");
-let touchCount = 0;
+let touchTimeout;
 
 img.addEventListener("touchstart", (event) => {
   event.preventDefault();
-  touchCount += event.touches.length;
-
-  if (touchCount === event.touches.length) {
-    requestAnimationFrame(() => {
-      img.style.transform = "scale(0.95)";
-    });
-  }
+  img.style.transform = "scale(0.95)";
 });
 
 img.addEventListener("touchend", (event) => {
   event.preventDefault();
-
-  touchCount -= event.changedTouches.length;
-
-  if (touchCount === 0) {
-    requestAnimationFrame(() => {
-      img.style.transform = "scale(1)";
-    });
-  }
+  img.style.transform = "scale(1)";
 
   Array.from(event.changedTouches).forEach((touch) => {
     const touchX = touch.clientX;
