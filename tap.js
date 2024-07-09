b7202d5141f1e2b184184cb2e51e1e1efc7bb171
amin-dev-tech <amin.dev.tech@gmail.com>
2024-07-09 17:02:29 +0300

    work on tap animation

--- tap.js
+++ tap.js
@@ -1,16 +1,41 @@
+// const img = document.getElementById("tap-effect");
+// let touchTimeout;
+
+// img.addEventListener("touchstart", (event) => {
+//   event.preventDefault();
+//   img.style.transform = "scale(0.95)";
+// });
+
+// img.addEventListener("touchend", (event) => {
+//   event.preventDefault();
+//   img.style.transform = "scale(1)";
+
+//   Array.from(event.changedTouches).forEach((touch) => {
+//     const touchX = touch.clientX;
+//     const touchY = touch.clientY;
+
+//     const plusOne = document.createElement("div");
+//     plusOne.className = "plus-one";
+//     plusOne.innerText = "+1";
+//     document.body.appendChild(plusOne);
+
+//     plusOne.style.left = `${touchX}px`;
+//     plusOne.style.top = `${touchY}px`;
+
+//     setTimeout(() => {
+//       plusOne.remove();
+//     }, 1000);
+//   });
+// });
+
 const img = document.getElementById("tap-effect");
 let touchTimeout;
 
 img.addEventListener("touchstart", (event) => {
   event.preventDefault();
   img.style.transform = "scale(0.95)";
-});
 
-img.addEventListener("touchend", (event) => {
-  event.preventDefault();
-  img.style.transform = "scale(1)";
-
-  Array.from(event.changedTouches).forEach((touch) => {
+  Array.from(event.touches).forEach((touch) => {
     const touchX = touch.clientX;
     const touchY = touch.clientY;
 
@@ -26,4 +51,13 @@ img.addEventListener("touchend", (event) => {
       plusOne.remove();
     }, 1000);
   });
+
+  touchTimeout = setTimeout(() => {
+    img.style.transform = "scale(1)";
+  }, 100);
+});
+
+img.addEventListener("touchend", (event) => {
+  clearTimeout(touchTimeout);
+  img.style.transform = "scale(1)";
 });
