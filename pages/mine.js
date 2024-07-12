e7e8d7d5511c48104e3bf05769750ee1cb97103d
amin-dev-tech <amin.dev.tech@gmail.com>
2024-07-12 17:16:32 +0300

    complete cards page

--- pages/mine.js
+++ pages/mine.js
@@ -2,38 +2,71 @@
 const myCardsLink = document.getElementById("my-cards-link");
 const newCardsLink = document.getElementById("new-cards-link");
 const flagCardsLink = document.getElementById("flag-cards-link");
+const specialsLink = document.getElementById("special-cards-link");
 
 // CARDS LISTS
 const myCardsList = document.getElementById("my-cards");
 const newCardsList = document.getElementById("new-cards");
 const flagCardsList = document.getElementById("flag-cards-list");
+const specialsList = document.getElementById("special-cards-list");
 
 myCardsLink.addEventListener("click", () => {
   myCardsList.classList.remove("hidden");
   flagCardsList.classList.add("hidden");
   newCardsList.classList.add("hidden");
+  specialsList.classList.add("hidden");
 
   myCardsLink.classList.add("active-cards");
   flagCardsLink.classList.remove("active-cards");
   newCardsLink.classList.remove("active-cards");
+  specialsLink.classList.remove("active-cards");
 });
 
 newCardsLink.addEventListener("click", () => {
   newCardsList.classList.remove("hidden");
   flagCardsList.classList.add("hidden");
+  specialsList.classList.add("hidden");
   myCardsList.classList.add("hidden");
 
   flagCardsLink.classList.remove("active-cards");
   newCardsLink.classList.add("active-cards");
   myCardsLink.classList.remove("active-cards");
+  specialsLink.classList.remove("active-cards");
 });
 
 flagCardsLink.addEventListener("click", () => {
-  myCardsLink.classList.remove("active-cards");
-  newCardsLink.classList.remove("active-cards");
-  flagCardsLink.classList.add("active-cards");
-
   flagCardsList.classList.remove("hidden");
   newCardsList.classList.add("hidden");
   myCardsList.classList.add("hidden");
+  specialsList.classList.add("hidden");
+
+  myCardsLink.classList.remove("active-cards");
+  newCardsLink.classList.remove("active-cards");
+  flagCardsLink.classList.add("active-cards");
+  specialsLink.classList.remove("active-cards");
+});
+
+specialsLink.addEventListener("click", () => {
+  myCardsList.classList.add("hidden");
+  newCardsList.classList.add("hidden");
+  flagCardsList.classList.add("hidden");
+  specialsList.classList.remove("hidden");
+
+  myCardsLink.classList.remove("active-cards");
+  newCardsLink.classList.remove("active-cards");
+  flagCardsLink.classList.remove("active-cards");
+  specialsLink.classList.add("active-cards");
+});
+
+// ADJUST CARD TITLE SIZE BASED ON TITLE WIDTH
+const cardTitle = document.querySelectorAll(".card-item h5");
+
+cardTitle.forEach((title) => {
+  if (title.textContent.length > 14 && title.textContent.length < 18) {
+    title.style.fontSize = "12px";
+  } else if (title.textContent.length > 18) {
+    title.style.fontSize = "11px";
+  } else {
+    title.style.fontSize = "";
+  }
 });
